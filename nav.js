cc4fef62e364587b7d63624ee766cdc4e5b7013a
RSMN574 <74419035+RSMN574@users.noreply.github.com>
2021-08-15 02:43:46 +0800

    Update nav.js

--- nav.js
+++ nav.js
@@ -10,7 +10,34 @@ const nav = Vue.createApp({
 // 子元件
 vm.component('nav', {
 	template: "
-  
+<ul class="menu-bar">
+	<li class="navdd">Android資訊
+	  <nav class="submenu">
+                <ul class="submenu-items">
+                    <li class="submenu-item"><a href="https://rsmn574.github.io/androidsys.html" class="submenu-link">Android系統</a></li>
+                    <li class="submenu-item"><a href="https://rsmn574.github.io/android12.html" class="submenu-link">Android12專區</a></li>
+                    <li class="submenu-item"><a href="#" class="submenu-link">Android APP</a></li>
+                </ul>
+            </nav>
+	</li>
+	<li class="navdd">Android資訊
+	  <nav class="submenu">
+                <ul class="submenu-items">
+                    <li class="submenu-item"><a href="https://rsmn574.github.io/androidsys.html" class="submenu-link">Android系統</a></li>
+                    <li class="submenu-item"><a href="https://rsmn574.github.io/android12.html" class="submenu-link">Android12專區</a></li>
+                    <li class="submenu-item"><a href="#" class="submenu-link">Android APP</a></li>
+                </ul>
+            </nav>
+	</li>
+  <li class="navdd">Android資訊
+	  <nav class="submenu">
+                <ul class="submenu-items">
+                    <li class="submenu-item"><a href="https://rsmn574.github.io/androidsys.html" class="submenu-link">Android系統</a></li>
+                    <li class="submenu-item"><a href="https://rsmn574.github.io/android12.html" class="submenu-link">Android12專區</a></li>
+                    <li class="submenu-item"><a href="#" class="submenu-link">Android APP</a></li>
+                </ul>
+            </nav>
+	</li>
   
   "
    
