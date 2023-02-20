ca4c270eb968c7baa19391da02a50a3eac62a626
nick <nickkool0319>
2023-02-21 01:15:30 +0800

    1.1

--- main.js
+++ main.js
@@ -8,16 +8,16 @@ const app = Vue.createApp({
   app.component('postcard', {
     template:` <template v-for="i in article">
     <a class="mt-8 max-h-48 w-6/12 sm:h-max sm:w-9/12 h-full justify-self-center md:w-9/12 justify-self-center" :href="(i.href)">
-	<div class="max-w-md h-full sm:h-max mx-auto shadow-md overflow-hidden border border-slate-300 bg-[#f5f8fc] dark:bg-[#1c1f26] dark:border-slate-700 rounded-xl md:max-w-2xl hover:scale-105 transition ease-in-out">
+	<div class="max-w-md h-full sm:h-max mx-auto shadow-md overflow-hidden border border-slate-300 bg-[#f5f8fc]  rounded-xl md:max-w-2xl hover:scale-105 transition ease-in-out">
 		<div class="md:flex">
 			<div class="md:shrink-0"><img alt="" class="h-full w-full object-cover md:h-full md:w-48 sm:h-1/4 aspect-video" :src="(i.IMG)"></div>
 			<div class="p-4">
-				<h1 class="block mt-1 text-lg leading-tight font-medium text-black dark:text-gray-200">{{i.title}}</h1>
+				<h1 class="block mt-1 text-lg leading-tight font-medium text-black ">{{i.title}}</h1>
 				<p class="mt-2 text-slate-500">{{i.introduction}}</p>
 				<div class="flex items-center gap-2 p-4">
 					<img class="rounded-full w-10 h-10" :src="(i.authorIMG)">
 					<div class="flex flex-col">
-						<strong class="text-center text-slate-900 text-base font-medium dark:text-slate-200 sm:text-sm">{{i.author}}</strong><span class="text-end text-slate-500 text-sm font-medium dark:text-slate-400">{{i.date}}</span>
+						<strong class="text-center text-slate-900 text-base font-medium  sm:text-sm">{{i.author}}</strong><span class="text-end text-slate-500 text-sm font-medium dark:text-slate-400">{{i.date}}</span>
 					</div>
 				</div>
 			</div>
@@ -64,36 +64,70 @@ this.category="flash"
    },
   })
   app.component('navbar', {
-  template:`<div class="navbar navbar-glass navbar-sticky sticky rounded-lg w-11/12 mx-auto mb-12">
-          <div class="navbar-start">
-              <a class="navbar-item" href="index.html"><font style="vertical-align: inherit;"><img class="inline w-6 mr-4" src="a-k-o-f.svg"><font style="vertical-align: inherit;">Android Tips</font></font></a>
-          </div>
-          <div class="navbar-center">
-          
-          <a class="navbar-item" href="flash.html">刷機專區</a>
-          <a class="navbar-item" href="beautification.html">美化手機</a>
-          <a class="navbar-item" href="internet.html">網路軟體</a>
-          <a class="navbar-item" href="media.html">影音娛樂</a>
-          <a class="navbar-item" href="other.html">其他</a>
-          <a class="navbar-item" href="aboutus.html">關於我們</a>
-      </div>
-      <div class="navbar-end">
-		<div class="avatar avatar-ring avatar-md">
-			<div class="dropdown-container">
-				<div class="dropdown">
-					<label class="btn btn-ghost flex cursor-pointer px-0" tabindex="0">
-					<span class="material-symbols-outlined">
-account_circle
-</span>
-					</label>
-					<div class="dropdown-menu dropdown-menu-bottom-left">
-						<a class="dropdown-item text-sm" href="login.html">登入</a>
-					
-					</div>
-				</div>
-			</div>
-		</div>
-      </div>`,
+  template:` <div class=" hidden md:inline h-16  fixed w-screen border-b border-slate-900/10 backdrop-blur-lg bg-white/30 h-12 rounded-sm ">
+  
+  
+
+  <ul class="md:flex lg:flex text-center justify-center gap-x-2 mt-5 hidden">
+      <a class="router-link-active router-link-exact-active" href="index.html">
+      <li class="inline-block "><img class="w-8" src="a-k-o-f.svg"></li>
+      </a>
+       
+        <li class="inline-block "><a class="navbar-item" href="flash.html">刷機專區</a></li>
+       
+        <li class="inline-block  "><a class="navbar-item" href="internet.html">網路軟體</a></li>
+       
+        <li class="inline-block  "><a class="navbar-item" href="beautification.html">美化手機</a></li>
+       
+        <li class="inline-block  "><a class="navbar-item" href="internet.html">網路軟體</a></li>
+       
+        <li class="inline-block  "><a class="navbar-item" href="media.html">影音娛樂</a></li>
+       
+        <li class="inline-block  "><a class="navbar-item" href="other.html">其他</a></li>
+        <li class="inline-block  "><a class="navbar-item" href="aboutus.html">關於我們</a></li>
+      </ul>
+    </div>
+ 
+</div>
+<div class="fixed md:hidden w-screen border-b border-slate-900/10 backdrop-blur-lg bg-white/30 h-14 rounded-sm ">
+<div class="mt-2 left-4 absolute"><a class="router-link-active router-link-exact-active" href="index.html">
+<img class="w-8" src="a-k-o-f.svg">
+</a></div>
+
+<div class="absolute right-4 mt-1"><input type="checkbox" id="drawer-left" class="drawer-toggle" />
+
+<label for="drawer-left" class="btn bg-gradient-to-r from-[#fa709a] to-[#fee140]"><span class="stroke-blue-500 material-symbols-outlined">
+menu
+</span></label>
+<label class="overlay" for="drawer-left"></label>
+<div class="drawer">
+	<div class="drawer-content pt-10 flex flex-col h-full">
+		<label for="drawer-left" class="btn btn-sm btn-circle btn-ghost absolute right-2 top-2">✕</label>
+    <ul class="">
+    
+     
+      <li class=" my-4 align-top"><a class="navbar-item" href="flash.html">刷機專區</a></li>
+     
+      <li class=" my-4   align-top"><a class="navbar-item" href="internet.html">網路軟體</a></li>
+     
+      <li class="  my-4  align-top"><a class="navbar-item" href="beautification.html">美化手機</a></li>
+     
+      <li class="  my-4  align-top"><a class="navbar-item" href="internet.html">網路軟體</a></li>
+     
+      <li class=" my-4  align-top"><a class="navbar-item" href="media.html">影音娛樂</a></li>
+     
+      <li class="  my-4  align-top"><a class="navbar-item" href="other.html">其他</a></li>
+      <li class="  my-4  align-top"><a class="navbar-item" href="aboutus.html">關於我們</a></li>
+    </ul>
+	</div>
+</div>
+</div>
+</div>
+  
+    
+   
+
+`,
     data(){
               return{
                  
